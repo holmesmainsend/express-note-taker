924d513e45661dc7ba1c77096d43103a1a75246b
Mona Lisa <holmesmainsend@gmail.com>
2021-12-05 10:07:08 -0500

    began adding get requests

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const noteData = require('./db/db.json');
+// const noteData = require('/db/db.json');
 
 const PORT = 3001;
 
@@ -8,3 +8,12 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+// app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
