30c7fc2026da8a2b2a262fdf7bbfdee2a2a9536b
Mona Lisa <holmesmainsend@gmail.com>
2021-12-06 18:43:02 -0500

    fixed file tree/path

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const path = require('path');
-// const noteData = require('/db/db.json');
+// const noteData = require('db/db.json');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 // app.get('/api/notes', (req, res) => res.json(noteData));
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`App listening at http://localhost:${PORT}`);
 });
