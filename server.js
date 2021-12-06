e05700f8c4992727e89b4a5e2bb184b11eb2f650
Mona Lisa <holmesmainsend@gmail.com>
2021-12-06 18:46:03 -0500

    fixed json retrieval

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const noteData = require('db/db.json');
+const noteData = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,7 +16,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-// app.get('/api/notes', (req, res) => res.json(noteData));
+app.get('/api/notes', (req, res) => res.json(noteData));
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
