244940ee18db98eb0e705082426999b654df4643
Mona Lisa <holmesmainsend@gmail.com>
2021-12-07 10:15:55 -0500

    added * route, fs, TODOs

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+// TODO: create/test POST route
+// TODO: connect Frontend to Backend
+// TODO: deploy to Heroku
+// TODO: add demo video + README
+
 const express = require('express');
 const path = require('path');
 const noteData = require('./db/db.json');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
@@ -17,6 +23,10 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
